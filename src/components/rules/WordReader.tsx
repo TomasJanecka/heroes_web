31903378ce04979911f82741e66c54fc434c30f1
TomasJanecka <tomas.janecka12@gmail.com>
2024-01-18 17:08:45 +0100

    add pravidla2

--- src/components/rules/WordReader.tsx
+++ src/components/rules/WordReader.tsx
@@ -2,7 +2,7 @@ import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
 export const WordReader = () => {
   const docs = [
-    { uri: require("./pravidlaMoroTest.pdf") }, // Local File
+    { uri: require("./pravidlaV2.pdf") }, // Local File
   ];
 
   return <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} />;
