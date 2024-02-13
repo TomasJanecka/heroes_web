2ab77edade93149bf22b7b63536c27c92895b0e7
Tomas Janecka <xjaneck1@fi.muni.cz>
2024-02-13 22:52:00 +0100

    update hero, card types.ts

--- src/heroes/types.ts
+++ src/heroes/types.ts
@@ -4,35 +4,89 @@ export type CardObject = {
   hero: Hero,
   image: string,
   type: CardType,
-  keywords: CardKeyword[],
-  target: CardType[],
-  effect?: CardEffect[],
+  cardColor: CardColor,
+  playRange: CardRange,
+  target: CardTarget[],
+  effect?: Effect[],
+  isUnit?: UnitType,
   isDeactivated?: boolean
-  description: string,
   count: number,
   aoe?: number;
-  additionalInfo?: string,
   precondition?: CrystalCardId[],
   afterEffect?: () => {} | undefined
+}
+
+export enum UnitType {
+  trap = "trap",
+  tower = "tower",
+  bomb = "bomb",
+}
+
+export enum CardRange {
+  attackR = "attackR",
+  castR = "castR",
+  yourPosition = "yourPosition"
+}
+
+export enum CardTarget {
+  enemy = "enemy",
+  ally = "ally",
+  self = "self",
+  hero = "hero",
+  unit = "unit",
+  arena = "arena",
+  card = "card"
+}
+
+export enum CardType {
+  card = "card",
+  hero = "hero",
+  unit = "unit",
+  battleground = "battleground" // specialny typ jendotky, hracia plocha
+}
+
+// tieto typy su na kartickach
+export enum CardSpecialization {
+  attack = "attack",
+  range = "range",
+  noBlack = "noBlack",
+  unit = "unit",
+  unitType = "unitType",
+  tower = "tower",
+  trap = "trap",
+  bomb = "bomb",
+  movement = "movement",
+  statusChange = "statusChange",
+  block = "block",
+  heal = "heal",
+  aoe = "aoe",
+  yourPosition = "yourPosition",
+}
+
+export enum CardColor {
+  red = "red",
+  blue = "blue",
+  black = "black",
+  light = "light",
 }
 
 export type HeroType = {
   id: Hero,
   name: string,
   image: string,
-  states?: CardEffect[], // stavy hrdinu
+  states?: HeroState[],  // stavy hrdinu
   position: number,
-  attackRange: number,
-  castRange: number,
+  attackRange: number,    // dosah utoku
+  castRange: number,      // dosah kuzla
   attack: number,
   hp: number,
   maxHp: number,
-  hand: CardObject[], // karty na ruke
-  deck: CardObject[], // tahaci balicek
-  discard: CardObject[], // odhadzovaci balicek
+  hand: CardObject[],     // karty na ruke
+  deck: CardObject[],     // tahaci balicek
+  discard: CardObject[],  // odhadzovaci balicek
   team: Team | undefined,
   movement: 1 | 2,
-  hasEffects?: string[],
+  hasEffects?: Effect[],
   heroPassive?: () => {} | undefined
 }
 
@@ -55,65 +109,6 @@ export enum CrystalCardId {
   crystalHero = "crystalHero",
 }
 
-export enum CardType {
-  card = "card",
-  hero = "hero",
-  unit = "unit",
-  self = "self",
-  enemy = "enemy",
-  ally = "ally",
-  battleground = "battleground" // specialny typ jendotky, hracia plocha
-}
-
-export enum CardKeyword {
-  red = "red",
-  blue = "blue",
-  black = "black",
-  light = "light",
-  attackR = "attackR",
-  castR = "castR",
-  attack = "attack",
-  other = "other",
-  move = "move",
-  unit = "unit",
-  trap = "trap",
-  tower = "tower",
-  bomb = "bomb",
-  enemy = "enemy",
-  ally = "ally",
-  arena = "arena",
-  self = "self",
-  card = "card",
-  discard = "discard",
-  buff = "buff",
-  disable = "disable",
-  block = "block",
-  noBlack = "noBlack",
-  movement = "movement",
-  statusChange = "statusChange",
-  heal = "heal",
-  aoe = "aoe",
-  yourPosition = "yourPosition"
-}
-
-// enum CardKeyword obsahuje vsetko z CardSpecialization
-export enum CardSpecialization {
-  attack = "attack",
-  range = "range",
-  noBlack = "noBlack",
-  unit = "unit",
-  unitType = "unitType",
-  tower = "tower",
-  trap = "trap",
-  bomb = "bomb",
-  movement = "movement",
-  statusChange = "statusChange",
-  block = "block",
-  heal = "heal",
-  aoe = "aoe",
-  yourPosition = "yourPosition",
-}
-
 export enum Team {
   A = "A", B = "B", C = "C",
 }
@@ -123,10 +118,19 @@ export enum Hero {
   toxic = "toxic",
 }
 
-export enum CardEffect {
+export enum HeroState {
   slow = "slow",
   disarm = "disarm",
   root = "root",
   silence = "silence",
+}
+
+export enum Effect {
   block = "block",
+  dispel = "dispel",
+  bonusHp = "bonusHp",
+  bonusAttack = "bonusAttack",
+  bonusCastRange = "bonusCastRange",
+  bonusAttackRange = "bonusCastRange",
+  reincarnation = "reincarnation"
 }
